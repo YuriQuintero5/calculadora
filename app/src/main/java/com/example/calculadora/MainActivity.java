f0146d42885b7a7a0392734b0e6a4da572240218
Yuri Quintero <ingenieraquintero@gmail.com>
2020-05-23 12:02:02 -0500

    Spinner agregado y demas opciones funcionando

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText numero_uno, numero_dos;
     private TextView resultado;//NULL
+    private Spinner combo_operaciones;
+    private String[] operaciones;
+    private ArrayAdapter<String> Adapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +29,30 @@ public class MainActivity extends AppCompatActivity {
         numero_uno = findViewById(R.id.txtNumeroUno);
         numero_dos = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.lbResultado);
-
+        combo_operaciones = findViewById(R.id.cmbOperaciones);
+        operaciones = getResources().getStringArray(R.array.operaciones);
+        Adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, operaciones);
+        combo_operaciones.setAdapter(Adapter);
     }
 
     public void calcular(View v) {
-        int num_uno, num_dos, suma;
+        int  op;
+        double num_uno, num_dos, res = 0;
         if(validar()) {
-            num_uno = Integer.parseInt(numero_uno.getText().toString());
-            num_dos = Integer.parseInt(numero_dos.getText().toString());
-            suma = num_uno + num_dos;
-            resultado.setText("" + suma);
+            num_uno = Double.parseDouble(numero_uno.getText().toString());
+            num_dos = Double.parseDouble(numero_dos.getText().toString());
+            op = combo_operaciones.getSelectedItemPosition();
+            if (op == 0){
+                res = num_uno + num_dos;
+            }else if(op == 1){
+                res = num_uno - num_dos;
+            }else if (op == 2){
+                res = num_uno * num_dos;
+            }else if (op == 3){
+                res = num_uno / num_dos;
+            }
+
+            resultado.setText("" + res);
         }
 
     }
@@ -39,23 +62,33 @@ public class MainActivity extends AppCompatActivity {
         numero_dos.setText("");
         resultado.setText("");
         numero_uno.requestFocus();
+        combo_operaciones.setSelection(0);
     }
 
     public boolean validar() {
-        String error_numero_uno, error_numero_dos;
+        String error_numero_uno, error_numero_dos, error_division;
+        int op;
+
         error_numero_uno = getResources().getString(R.string.error_numero_uno);
         error_numero_dos = getResources().getString(R.string.error_numero_dos);
+        error_division = getResources().getString(R.string.error_division);
+
+        op=combo_operaciones.getSelectedItemPosition();
+
         if (numero_uno.getText().toString().isEmpty()) {
-            numero_uno.setError("Digite el numero uno");
+            numero_uno.setError(error_numero_uno);
             numero_uno.requestFocus();
             return false;
-        } else {
-            if (numero_dos.getText().toString().isEmpty()) {
-                numero_dos.setError("Digite el numero dos");
+        } else if (numero_dos.getText().toString().isEmpty()) {
+                numero_dos.setError(error_numero_dos);
+                numero_dos.requestFocus();
+                return false;
+            }else if (op == 3 && Double.parseDouble(numero_dos.getText().toString())== 0){
+                numero_dos.setError(error_division);
                 numero_dos.requestFocus();
                 return false;
             }
             return true;
         }
     }
-}
+
